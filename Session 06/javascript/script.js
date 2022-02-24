2b6903200fb0c2b91ecb936adb2f54720f558807
navidtrc <navidtrc@gmail.com>
2022-02-24 14:00:05 +0330

    session 6

---
+++ Session 06/javascript/script.js
@@ -0,0 +1,123 @@
+// // var age = 26;
+
+// // // 1- declares (creates) the variable
+// // // 2- assign it an itinial value
+
+// // age = 21;
+
+// // var number;
+// // number = 10;
+
+// // string رشته
+// var name = 'navid tafreshi';
+// var name1 = "jack jones";
+// // '   ' => single cotation
+// // "   " => double cotation
+
+// name = 'reza ahmadi';
+
+// // name = 10;
+
+// // name = false;
+
+// // boolean
+// var isSuccesfull = true;
+// isSuccesfull = false;
+
+// // number عدد
+// var number = 25;
+// number = 12.5;
+// number = -92;
+// number = 4 * 32
+
+// number = Infinity;
+// number = -Infinity;
+// number = NaN; // => Not a number;
+// number = 78;
+
+// var num1 = 20;
+// var num2 = 88;
+// // همیشه از راست تو چپ ریخته میشه
+// num1 = num2;
+// // console.log("Hello javascript class")
+
+// // console.log(name1)    // ======> 'reza ahmadi'
+// // console.log("name dajlgakdufhgkadjfhgukadfhg")  // ======> 'name'
+// // console.log(10 + 2 * 4);
+
+// var result = true;
+
+// // condition => شرط => true  / false   => یا میشه یا نمیشه
+
+// result = number > 78; // 78 > 78 بزرگتر
+// result = number >= 78; // بزرگتر مساری
+// result = number <= 78; // کوچکتر مساوری
+// result = number < 78; // کوچکتر
+// result = number === 78; // برابر
+// result = number !== 10; // نا برابر
+
+// result = number + 9;
+
+// var myvar1 = 5;
+// var myvar2 = "5";
+
+// var threeEquals = myvar1 === myvar2; // value and type
+// var twoEquals = myvar1 == myvar2; // just value
+
+// // var result = "5" + "10"
+// var firstName = "Navid";
+// var lastName = "Tafreshi";
+// var age = 26;
+
+// var fullName = firstName + " " + lastName;
+
+// //       Navid Tafreshi       has     26      years old
+// var about = fullName    +   " has " + age + " years old";
+
+var result = 10 > 2;
+
+// if (condition) {
+//      ... body
+//}
+
+var average = 19.2;
+// condition:  true / false
+// if (average >= 18) {
+//     console.log("BUY");
+// }
+// if (average < 18) {
+//     console.log("DONT BUY")
+// }
+
+// if (average >= 18) {
+//     console.log("BUY");
+// } else {
+//     console.log("DONT BUY")
+// }
+
+// if (average >= 18) {
+//   console.log("iPhone");
+// } else if (average >= 16) {
+//   console.log("PS5");
+// } else {
+//   console.log("Chak afsari");
+// }
+
+// if (average >= 18) {
+//   console.log("iPhone");
+// }
+// if (average >= 16) {
+//   console.log("PS5");
+// } else {
+//   console.log("Chak afsari");
+// }
+
+// var sum = 10 + 2; // 12
+// var mines = 10 - 2; // 8
+// var division = 10 / 2; // 5
+// var multiple = 10 * 2; // 20;
+// var remain = 23 % 3; // 2
+// var result = (5 * (6 + 1 - 4)) / 2;
+
+var input = prompt("Enter your number?");
+alert("your input is " + input);
